9cb5538137f3ca6f8ecdbd7fbb6a7190704d6440
drxzcl <drxzclx@gmail.com>
2016-12-12 23:15:03 +0100

    Add support for accelerometer on MPU925x

--- wifisensor/mpu925x.h
+++ wifisensor/mpu925x.h
@@ -3,7 +3,8 @@ class MPU925x: public I2CSensor {
     MPU925x(int i2cAddr):I2CSensor(i2cAddr){ }
     virtual void begin(){}
     virtual float readValue(int kind) {
-        MPU9250 myIMU;
+      MPU9250 myIMU;
+      if (kind==I2CSENSOR_VALUE_KIND_TEMP) {
         byte c = myIMU.readByte(this->i2cAddr, WHO_AM_I_MPU9250);
         myIMU.tempCount = myIMU.readTempData();  // Read the adc values
         // Temperature in degrees Centigrade
@@ -12,7 +13,31 @@ class MPU925x: public I2CSensor {
         Serial.print("Temperature is ");  Serial.print(myIMU.temperature, 1);
         Serial.println(" degrees C");
     
-        return myIMU.temperature;
+        return myIMU.temperature;        
+      } else if (kind==I2CSENSOR_VALUE_KIND_ACC_TOTAL) {
+         
+         float acc2;
+         
+         myIMU.readAccelData(myIMU.accelCount);  // Read the x/y/z adc values
+         myIMU.getAres();
+         // Now we'll calculate the accleration value into actual g's
+         // This depends on scale being set
+         myIMU.ax = (float)myIMU.accelCount[0]*myIMU.aRes; // - accelBias[0];
+         myIMU.ay = (float)myIMU.accelCount[1]*myIMU.aRes; // - accelBias[1];
+         myIMU.az = (float)myIMU.accelCount[2]*myIMU.aRes; // - accelBias[2];
+
+         acc2 =  myIMU.ax *  myIMU.ax;
+         acc2 +=  myIMU.ay *  myIMU.ay;
+         acc2 +=  myIMU.az *  myIMU.az;
+
+         float acc = sqrt(acc2);
+         Serial.print("Total acc: ");Serial.println(acc);
+         return sqrt(acc2);
+
+      } else {
+        Serial.println("Unsupported 'kind' on MPU925x");
+        return -1.0;
+      }
     }
   private:
     
